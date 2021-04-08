097c9af9d60f48cee9dc03f37c354c43df7008a7
rgneville <rgneville91@gmail.com>
2021-04-08 23:02:21 +0100

    Convert to SASS, styling improvements

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 const submitButton = document.querySelector('#submit-search');
-let searchTerm = document.querySelector('#search-box').value;
+const errorBox = document.querySelector('#errorBox');
 const cntnr = document.querySelector('#container');
+const locations = [
+    "Thornbury, GB",
+    "Brasilia"
+];
 
-function createWeatherCard () {
-    searchTerm = document.querySelector('#search-box').value;
-    let weather = getWeather();
-    weather.then(function(result) {
-        buildCard(result);
-    })
-    weather.catch(function(error) {
-        console.log(error);
-        return 404
-    })    
+function clearDisplay () {
+    cntnr.innerHTML = '';
+}
+
+function addLocation () {
+    let newLocation = document.querySelector('#search-box').value;
+    locations.push(newLocation);
+}
+
+function createWeatherCards () {
+    for (let i = 0; i < locations.length; i++) {
+        let weather = getWeather(locations[i]);
+        weather.then(function(result) {
+            buildCard(result);
+        })
+        weather.catch(function(error) {
+            console.log(error);
+            return 404
+        })
+    }    
 }
 
 function buildCard (result) {
@@ -19,18 +33,30 @@ function buildCard (result) {
 
     const cardContainer = document.createElement('div');
     cardContainer.classList.add('cardbox');  
-    const nameContainer = document.createElement('div');
-    nameContainer.classList.add('namebox');
-    nameContainer.innerHTML = `${weatherJSON.name}, ${weatherJSON.sys.country}`;
+    const leftContainer = document.createElement('div');
+    leftContainer.classList.add('leftContainer');
+    
+    const rightContainer = document.createElement('div');
+    rightContainer.classList.add('rightContainer');
+
+    const city = document.createElement('div');
+    city.classList.add('city');
+    city.innerHTML = `<h2>${weatherJSON.name}, ${weatherJSON.sys.country}</h2>`;
+
     const tempContainer = document.createElement('div');
+    tempContainer.classList.add('tempContainer');
+
+    const currentTempDiv = document.createElement('div');
+    currentTempDiv.classList.add('currentTempDiv');
     let currentTemp = weatherJSON.main.temp.toFixed(1);
-    tempContainer.innerHTML = `current temp: ${currentTemp}`;
+    currentTempDiv.innerHTML = `${currentTemp}`;
+
     const feelsLikeContainer = document.createElement('div');
     let feelsLikeTemp = weatherJSON.main.feels_like.toFixed(1);
-    feelsLikeContainer.innerHTML = `feels like: ${feelsLikeTemp}`;
+    feelsLikeContainer.innerHTML = `feels like: ${feelsLikeTemp}°C`;
+
     const humidity = document.createElement('div');
-    humidity.innerHTML = `humidity: ${weatherJSON.main.humidity}%`
-    tempContainer.innerHTML = `current temp: ${currentTemp}`;
+    humidity.innerHTML = `humidity: ${weatherJSON.main.humidity}%`;
     const icon = document.createElement('img');
     iconCode = weatherJSON.weather[0].icon;
     icon.src = `http://openweathermap.org/img/wn/${iconCode}.png`
@@ -44,31 +70,40 @@ function buildCard (result) {
             currentTemp = currentTemp * (9/5) + 32;
             feelsLikeTemp = feelsLikeTemp * (9/5) + 32;
             cToF.innerHTML = "°F";
-            tempContainer.innerHTML = `current temp: ${currentTemp.toFixed(1)}`;
-            feelsLikeContainer.innerHTML = `feels like: ${feelsLikeTemp.toFixed(1)}`;
+            currentTempDiv.innerHTML = `${currentTemp.toFixed(1)}`;
+            feelsLikeContainer.innerHTML = `feels like: ${feelsLikeTemp.toFixed(1)}°F`;
         } else if (cToF.innerHTML === "°F") {
             currentTemp = (currentTemp - 32) * 5/9;
             feelsLikeTemp = (feelsLikeTemp - 32) * 5/9;
             cToF.innerHTML = "°C";
-            tempContainer.innerHTML = `current temp: ${currentTemp.toFixed(1)}`;
-            feelsLikeContainer.innerHTML = `feels like: ${feelsLikeTemp.toFixed(1)}`;
+            currentTempDiv.innerHTML = `${currentTemp.toFixed(1)}`;
+            feelsLikeContainer.innerHTML = `feels like: ${feelsLikeTemp.toFixed(1)}°C`;
         }
     })
 
-    cardContainer.appendChild(nameContainer);
-    cardContainer.appendChild(tempContainer);
-    cardContainer.appendChild(feelsLikeContainer);
-    cardContainer.appendChild(humidity);
-    cardContainer.appendChild(icon);
-    cardContainer.appendChild(descContainer);
-    cardContainer.appendChild(cToF);
+    cardContainer.appendChild(leftContainer);
+    cardContainer.appendChild(rightContainer);
+    leftContainer.appendChild(city);
+    leftContainer.appendChild(tempContainer);
+    tempContainer.appendChild(currentTempDiv);
+    tempContainer.appendChild(cToF);
+    rightContainer.appendChild(feelsLikeContainer);
+    rightContainer.appendChild(humidity);
+    rightContainer.appendChild(icon);
+    rightContainer.appendChild(descContainer);
     cntnr.appendChild(cardContainer);
 }
 
-async function getWeather () {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchTerm}&appid=221fcdab1d58f471292073096b454d21&units=metric`, {mode: 'cors'})
+async function getWeather (location) {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=221fcdab1d58f471292073096b454d21&units=metric`, {mode: 'cors'})
     const weatherData = await response.json();
     return weatherData;
 }
 
-submitButton.addEventListener('click', createWeatherCard);
+submitButton.addEventListener('click', () => {
+    addLocation();
+    clearDisplay();
+    createWeatherCards();
+});
+
+createWeatherCards(locations);
